7ebbddc0f6f0128d830d2ec5c5a672a809fa543d
stmarkjonathan <stmarkjonathan@gmail.com>
2023-10-17 23:26:01 -0400

    implemented mutex to only allow one instance of app

--- App.xaml.cs
+++ App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -11,5 +12,18 @@ namespace TimezoneApp
 
     public partial class App : Application
     {
+        private Mutex _mutex;
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            bool isNewInstance = false;
+            _mutex = new Mutex(true, "ExtendedClipboard", out isNewInstance);
+
+            if (!isNewInstance)
+            {
+                App.Current.Shutdown();
+            }
+
+            base.OnStartup(e);
+        }
     }
 }
